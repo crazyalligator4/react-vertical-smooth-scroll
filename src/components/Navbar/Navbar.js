b6bf94a0f425490cebee421ad1effc9d6400d59c
Crazy Alligator <crazy.alligator4@gmail.com>
2021-08-05 09:37:44 -0300

    implementing real navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 // dependencies
 import React from 'react';
 import {Link as LinkR} from 'react-router-dom';
+import {Link as LinkS} from 'react-scroll';
+import {FaBars} from 'react-icons/fa';
 // components
 import './Navbar.css';
 
@@ -8,9 +10,15 @@ export default function Navbar() {
   return (
     <nav className='Nav'>
       <div className='NavbarContainer'>
-        <LinkR to='/' className='NavLogo'>
-          Dolla
-        </LinkR>
+        <LinkR to='/' className='NavLogo'>Vertical Smooth</LinkR>
+        <div className='MobileIcon'>
+          <FaBars/>
+        </div>
+        <ul className='NavMenu'>
+          <li className='NavItem'>
+            <LinkS to='about' className='NavLinks'>About</LinkS>
+          </li>
+        </ul>
       </div>
     </nav>
   )
