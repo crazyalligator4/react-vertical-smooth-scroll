091b8ceda19864c1b457e17372f205ac9d6e2371
Crazy Alligator <crazy.alligator4@gmail.com>
2021-08-05 20:14:17 -0300

    configure sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // dependencies
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 // components
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter as Router} from 'react-router-dom';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <Router className="App">
+      <Sidebar/>
       <Navbar/>
     </Router>
   );
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,38 @@
+// dependencies
+import React from 'react';
+import {Link as LinkS} from 'react-scroll';
+import {Link as LinkR} from 'react-router-dom';
+// components
+import './Sidebar.css';
+import {FaTimes} from 'react-icons/fa';
+
+const Sidebar = () => {
+  return (
+    <aside className='SidebarContainer'>
+      <div className='Icon'> 
+        <FaTimes className='CloseIcon'></FaTimes>
+      </div>
+      <div lassName='SidebarWrapper'>
+        <ul className='SidebarMenu'>
+          <LinkS to='about' className='SidebarLink'>
+            About
+          </LinkS>
+          <LinkS to='discover' className='SidebarLink'>
+            Discover
+          </LinkS>
+          <LinkS to='services' className='SidebarLink'>
+            Services
+          </LinkS>
+          <LinkS to='signup' className='SidebarLink'>
+            Sign Up
+          </LinkS>
+        </ul>
+        <div className='SideBtnWrap'>
+          <LinkR className='SidebarRoute' to='/signin'>Sign In</LinkR>
+        </div>
+      </div>
+    </aside>
+  )
+}
+
+export default Sidebar
