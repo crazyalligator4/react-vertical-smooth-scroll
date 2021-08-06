3f77be926787f93b18b7700bdb66b7558b220549
Crazy Alligator <crazy.alligator4@gmail.com>
2021-08-06 19:01:47 -0300

    hero section implement content

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,10 +1,17 @@
 // dependencies
-import React from 'react';
+import React, {useState} from 'react';
 // components
 import './HeroSection.css';
+import {Link as LinkS} from 'react-scroll';
 import Video from '../../videos/video.mp4';
+import {MdArrowForward, MdKeyboardArrowRight} from 'react-icons/md';
 
-function HeroSection() {
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  }
+
   return (
     <div className='HeroContainer'>
       <div className='HeroBackground'>
@@ -13,7 +20,25 @@ function HeroSection() {
           src={Video} type='video/mp4'
           className='VideoBackground'
         />
-      </div>  
+      </div>
+      <div className='HeroContent'>
+        <h1 className='HeroH1'>Header 1</h1>
+        <p className='HeroP'>Sign Up for a free acount and receive $15</p>
+        <div className='HeroBtnWraper'>
+          <LinkS
+            to='signup'
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            className='Button'
+          >
+            Get Started {
+            hover
+              ? <MdArrowForward className='ArrowForward'/>
+              : <MdKeyboardArrowRight className='ArrowRight'/>
+            }
+          </LinkS>
+        </div>
+      </div>
     </div>
   )
 }
