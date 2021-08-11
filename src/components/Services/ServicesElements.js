f41a0cf436fb670b0e51b74eeb66ea73d32f5655
Crazy Alligator <crazy.alligator4@gmail.com>
2021-08-11 12:42:35 -0300

    fix cards overflow on features

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -18,12 +18,12 @@ export const ServicesContainer = styled.div`
 `
 export const ServicesWrapper = styled.div`
   max-width: 1000px;
-  margin: 0 auto;
+  // margin: 0 auto;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
   align-items: center;
   grid-gap: 16px;
-  padding: 0 50px;
+  // padding: 0 50px;
 
   @media screen and (max-width: 1000px) {
     grid-template-columns: 1fr 1fr 1fr;
