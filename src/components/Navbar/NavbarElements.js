b7e67b6b6ed2ecb2eac314cfb2dbdd3bc18283e3
Crazy Alligator <crazy.alligator4@gmail.com>
2021-08-10 23:59:36 -0300

    burger menu centered

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -43,9 +43,9 @@ export const MobileIcon = styled.div`
 
   @media screen and (max-width: 768px) {
     display: block;
-    position: absolute;
-    top: 0;
-    right: 0;
+    // position: absolute;
+    // top: 0;
+    // right: 0;
     transform: translate(-100, 60%);
     font-size: 1.8rem;
     cursor: pointer;
